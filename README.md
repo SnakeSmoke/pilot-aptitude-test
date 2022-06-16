2703766c18662d88ed16dd5c695675b42d15bc17
SnakeSmoke <ormos@live.se>
2022-06-16 06:54:25 +0200

    move code from old project to new repo

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # pilot-aptitude-test
+
+How to run: 
+
+```
+python3 counttest.py <num> <difficulty>
+```
+
+The correct guesses will be located in facit.txt
---
+++ counttest.py
@@ -0,0 +1,37 @@
+import pyttsx3
+import concurrent.futures
+import sys
+import os
+from time import sleep
+from random import random, randrange
+
+DIFFICULTY = {
+    "hard": 1.0,
+    "medium": 2.5,
+    "easy": 5.0
+    }
+
+
+random_number_list = []
+for n in range(int(sys.argv[1])):
+    random_number_list.append(randrange(1,9))
+
+print(random_number_list)
+
+f = open("facit.txt", "w")
+f.write(f'RANDOM NUMBER LIST: {random_number_list}\n')
+index = 1
+while random_number_list:
+    a = random_number_list.pop(0)
+    os.system(f'say {str(a)}') 
+    sleep(DIFFICULTY[sys.argv[2]])
+    if not random_number_list:
+        break
+    b = random_number_list[0]
+    sum = a + b
+    counter = 0
+
+    f.write(f'{index}. ({a}+{b}) = {sum}\n')
+    index+=1
+
+f.close()
